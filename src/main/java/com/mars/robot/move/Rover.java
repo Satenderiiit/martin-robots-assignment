b2b4c97d48da61d6f6d4efabb51482be29845b43
Satenderiiit <satender.iiit@gmail.com>
2021-11-08 16:04:41 +0000

    Added turnLeft functionality

---
+++ src/main/java/com/mars/robot/move/Rover.java
@@ -0,0 +1,52 @@
+package com.mars.robot.move;
+
+
+public class Rover {
+
+    private static final int MAX_X_COORDINATE = 50;
+    private static final int MAX_Y_COORDINATE = 50;
+    public int xAxis;
+    public int yAxis;
+    public String direction;
+    public String robotStatus="";
+    private String[] message;
+
+
+    public Rover(String location){
+        this.message = location.replaceAll("^\\s+","").split(" "); // remove whitespace as prefix & split
+        this.xAxis = Integer.parseInt(message[0]);
+        this.yAxis = Integer.parseInt(message[1]);
+        this.direction = message[2];
+    }
+
+    public void printInstruction(){
+        System.out.println("xAxis "+xAxis);
+        System.out.println("yAxis "+yAxis);
+        System.out.println("direction "+direction);
+    }
+
+
+
+    public String turnLeft(){
+        switch(direction)
+        {
+            case "N":
+                return direction="W";
+
+            case "W":
+                return direction="S";
+
+            case "S":
+                return direction="E";
+
+            case "E":
+                return direction="N";
+
+            default:
+                throw new IllegalArgumentException();
+
+        }
+    }
+
+
+}
---
+++ src/test/java/com/mars/robot/move/RoverTest.java
@@ -0,0 +1,24 @@
+package com.mars.robot.move;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.rules.ExpectedException;
+import static org.junit.Assert.assertEquals;
+
+public class RoverTest {
+    private Rover rover;
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Test
+    @DisplayName("should turns left 90 degrees and remains on the current grid point.")
+    public void shouldTurnLeft(){
+        rover = new com.mars.robot.move.Rover("1 1 E");
+        rover.printInstruction();
+        rover.turnLeft();
+        assertEquals("N", rover.direction);
+
+    }
+
+}
